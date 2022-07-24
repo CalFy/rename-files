3bcdb8bccbfdfecc9c1cb080796cf824e71c45f6
CalFy <2006nak@gmail.com>
2022-07-24 16:33:33 +0300

    Add date to file name

--- renameFiles.py
+++ renameFiles.py
@@ -1,11 +1,10 @@
 import os
-import sys
+from datetime import date
 
+today = date.today()
 folder = r'D:\users\nikita\rename\papka'
-count = 1
 for file_name in os.listdir(folder):
         source = os.path.join(folder, file_name)
-        destination = os.path.join(folder, file_name + str(count))
+        destination = os.path.join(folder, file_name + today.strftime("%d.%m.%Y"))
         print(destination)
         os.rename(source, destination)
-
