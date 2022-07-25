a37f6806936f4f95de6ee01213c08dafa7a6330b
CalFy <2006nak@gmail.com>
2022-07-25 14:03:36 +0300

    add date of file creation

--- renameFiles.py
+++ renameFiles.py
@@ -1,11 +1,15 @@
 import os
 from datetime import date
+import datetime
 
 today = date.today()
 folder = r'D:\users\nikita\rename\papka'
 for file_name in os.listdir(folder):
+        c_date = os.path.getctime(folder)
+        dt_c = datetime.datetime.fromtimestamp(c_date).date()
         source = os.path.join(folder, file_name)
         name, ext = os.path.splitext(file_name)
-        destination = os.path.join(folder, name + today.strftime("%d_%m_%Y") + ext)
+        destination = os.path.join(folder, name + str(dt_c) + ext)
         os.rename(source, destination)
         print(destination)
+
