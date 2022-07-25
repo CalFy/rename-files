50ead970797b07d58ceb3f59d006b2042b414c83
CalFy <2006nak@gmail.com>
2022-07-25 17:17:58 +0300

    add color text in console

--- renameFiles.py
+++ renameFiles.py
@@ -1,8 +1,8 @@
 import os
 from datetime import date
 import datetime
+from colorama import Fore
 
-today = date.today()
 folder = r'D:\users\nikita\rename\papka'
 for file_name in os.listdir(folder):
         c_date = os.path.getctime(folder)
@@ -11,5 +11,9 @@ for file_name in os.listdir(folder):
         name, ext = os.path.splitext(file_name)
         destination = os.path.join(folder, name + str(dt_c) + ext)
         os.rename(source, destination)
-        print(destination)
+        if os.path.isfile(os.path.join(folder, file_name)):
+                print(Fore.GREEN+ destination)
+        else:
+                print(Fore.BLUE + destination)
+
 
